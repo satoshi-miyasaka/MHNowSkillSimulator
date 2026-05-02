5b7a5bb08399a1b615dee7a4e5af34219d3e1904
satoshi-miyasaka <satoshi.miyasaka.1000@gmail.com>
2026-05-02 19:56:43 +0900

    update: 入力値を少数点入れないで良いように修正

--- app/src/main.js
+++ app/src/main.js
@@ -39,31 +39,31 @@ document.querySelector('#app').innerHTML = `
   </div>
   <div>
   <br />
-  物理攻撃力<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a" /> = <br />
    ( (武器攻撃力<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a1" />
 + スタイル強化値<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a2" value="0" />)
-    × 攻撃力倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a3" value="1" />
+    × 攻撃力倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a3" value="100" />%
      ＋ 攻撃力UP<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a4" value="0" />
 + 錬成パラメータ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a5" value="0" />)
-      × 攻撃活性<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a6" value="1" />
+      × 攻撃活性<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a6" value="100" />%
 <br />
-  属性攻撃力<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b" /> = <br />
    ( (武器属性値<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b1" />
 + スタイル強化値<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b2" value="0" />)
-      × 属性倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b3" value="1" />
+      × 属性倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b3" value="100" />%
   + 属性攻撃力UP<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b4" value="0" />)
-  × 古龍属性倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b5" value="1" />
-  × 弱点補正倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b6" value="1" />
+  × 古龍属性倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b5" value="100" />%
+  × 弱点補正倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b6" value="100" />%
 <br />
-ダメージ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c" /> = <br />
-( 物理攻撃力(1) + 属性攻撃力(2) )
-    × ダメージ倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c1" value="1" />
-    × 武器固有補正<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c2" value="1" />
-        × 会心倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c3" value="1" />
-      × 武器SP倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c4" value="1" />
-    × 状態異常補正<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c5" value="1" />
-            × 肉質<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c6" value="100" />/100
-    × モーション値<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c7" value="100" />/100
+( 物理攻撃力(<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a" />)
++ 属性攻撃力(<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b" />) )
+    × ダメージ倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c1" value="100" />%
+    × 武器固有補正<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c2" value="100" />%
+        × 会心倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c3" value="100" />%
+      × 武器SP倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c4" value="100" />%
+    × 状態異常補正<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c5" value="100" />%
+            × 肉質<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c6" value="130" />/100
+    × モーション値<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c7" value="27" />/100
+<br />
+= ダメージ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c" />
 <br />
 ※ トレーニングエリアでのダメージを想定しています
   </div>
@@ -76,40 +76,40 @@ document.querySelector('#app').innerHTML = `
 const obj = document.querySelectorAll('.inputNumeric');
 obj.forEach((inputNumeric) => {
   inputNumeric.addEventListener('input', () => {
-    inputNumeric.value = inputNumeric.value.replace(/\D/g, '')
+    inputNumeric.value = inputNumeric.value.replace(/[\D]/g, '')
 
     const a1 = Number(document.getElementById('a1').value);
     const a2 = Number(document.getElementById('a2').value);
-    const a3 = Number(document.getElementById('a3').value);
+    const a3 = Number(document.getElementById('a3').value) / 100;
     const a4 = Number(document.getElementById('a4').value);
     const a5 = Number(document.getElementById('a5').value);
-    const a6 = Number(document.getElementById('a6').value);
+    const a6 = Number(document.getElementById('a6').value) / 100;
 
     const aObj = document.getElementById('a');
-    let a = ((a1 + a2) * a3 + a4 + a5) * a6
+    let a = Math.floor(Math.floor((a1 + a2) * a3 + a4 + a5) * a6);
     aObj.value = a;
 
     const b1 = Number(document.getElementById('b1').value);
     const b2 = Number(document.getElementById('b2').value);
-    const b3 = Number(document.getElementById('b3').value);
+    const b3 = Number(document.getElementById('b3').value) / 100;
     const b4 = Number(document.getElementById('b4').value);
-    const b5 = Number(document.getElementById('b5').value);
-    const b6 = Number(document.getElementById('b6').value);
+    const b5 = Number(document.getElementById('b5').value) / 100;
+    const b6 = Number(document.getElementById('b6').value) / 100;
 
     const bObj = document.getElementById('b');
-    let b  = ((b1 + b2) * b3 + b4) * b5 * b6
+    let b  = Math.floor(Math.floor(((b1 + b2) * b3 + b4) * b5) * b6);
     bObj.value = b;
 
-    const c1 = Number(document.getElementById('c1').value);
-    const c2 = Number(document.getElementById('c2').value);
-    const c3 = Number(document.getElementById('c3').value);
-    const c4 = Number(document.getElementById('c4').value);
-    const c5 = Number(document.getElementById('c5').value);
-    const c6 = Number(document.getElementById('c6').value);
-    const c7 = Number(document.getElementById('c7').value);
+    const c1 = Number(document.getElementById('c1').value) / 100;
+    const c2 = Number(document.getElementById('c2').value) / 100;
+    const c3 = Number(document.getElementById('c3').value) / 100;
+    const c4 = Number(document.getElementById('c4').value) / 100;
+    const c5 = Number(document.getElementById('c5').value) / 100;
+    const c6 = Number(document.getElementById('c6').value) / 100;
+    const c7 = Number(document.getElementById('c7').value) / 100;
 
     const cObj = document.getElementById('c');
-    let c = (a + b) * c1 * c2 * c3 * c4 * c5 * (c6 / 100) * (c7 / 100);
+    let c = Math.ceil((a + b) * c1 * c2 * c3 * c4 * c5 * c6 * c7);
     cObj.value = c;
   })
 })
