d65ab7b6cedddadbbd65c1fb8bba1ea71cc0bf75
satoshi-miyasaka <satoshi.miyasaka.1000@gmail.com>
2026-05-03 00:13:47 +0900

    update: レイアウト大体出来た

--- app/src/main.js
+++ app/src/main.js
@@ -16,74 +16,130 @@ document.querySelector('#app').innerHTML = `
     <button class="RenNothng">攻撃</button>
     <button class="RenExist">攻撃</button>
     <button class="RenOnry">攻撃</button>
-    <hr />
   </div>
-  <div id="SkillSelect">
+  <hr />
+  <div>
     <table>
+      <tr><th colspan="3">頭</th><th colspan="3">胴</th><th colspan="3">腕</th><th colspan="3">腰</th><th colspan="3">足</th></tr>
       <tr>
-        <td>削除</td><td>スキル</td><td>レベル</td><td>効果</td><td></td>
+        <td><input type="radio" name="head" checked="true" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
+        <td><input type="radio" name="body" checked="true" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
+        <td><input type="radio" name="arm" checked="true" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
+        <td><input type="radio" name="waist" checked="true" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
+        <td><input type="radio" name="foot" checked="true" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
       </tr>
       <tr>
-        <td><button class="SkillRemove">×</button></td>
-        <td>攻撃</td>
-        <td><select><option>Lv.1</option><option>Lv.2</option><option>Lv.3</option><option>Lv.4</option><option>Lv.5</option></select></td>
-        <td></td><td></td>
+        <td><input type="radio" name="head" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
+        <td><input type="radio" name="body" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
+        <td><input type="radio" name="arm" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
+        <td><input type="radio" name="waist" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
+        <td><input type="radio" name="foot" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
       </tr>
       <tr>
-        <td><button class="SkillRemove">×</button></td>
-        <td>攻撃</td>
-        <td><select><option>Lv.1</option><option>Lv.2</option><option>Lv.3</option><option>Lv.4</option><option>Lv.5</option></select></td>
-        <td></td><td></td>
+        <td><input type="radio" name="head" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
+        <td><input type="radio" name="body" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
+        <td><input type="radio" name="arm" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
+        <td><input type="radio" name="waist" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
+        <td><input type="radio" name="foot" /></td>
+        <td>自由枠<br /><select><option>G1</oprion></select></td>
+        <td> <div>攻撃:1</div> <div>憑依錬成:1</div> </td>
       </tr>
     </table>
   </div>
-  <div>
-  <br />
-  <table>
-    <tr>
-      <td>攻撃力</td><td><input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a1" /></td>
-    </tr>
-    <tr>
-      <td>属性値</td><td><input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b1" /></td>
-    </tr>
-    <tr>
-      <td>肉質</td><td><input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c6" value="130" /></td>
-    </tr>
-    <tr>
-      <td>モーション値</td><td><input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c7" value="27" /></td>
-    </tr>
-  </table>
   <hr />
-    ( 攻撃力 × 攻撃力<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a3" value="0" />%UP
-     ＋ 攻撃力<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a4" value="0" />UP
-+ 錬成パラメータ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a5" value="0" />)
-      × 攻撃活性<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a6" value="0" />%UP
-  <hr />
-   ( 属性値 × 属性値<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b3" value="0" />%UP
-  + 属性値<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b4" value="0" />UP)
-  × 古龍属性<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b5" value="0" />%UP
-  <hr />
-( 物理攻撃力<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a" />
-+ 属性攻撃力<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b" />)
-    × ダメージ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c1" value="0" />%UP
-    × 肉質 / 100
-    × モーション値 / 100
-<hr />
-= 基本ダメージ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c" />
-<br />
-基本ダメージ × 会心倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c3" value="1.25" />
-= 会心ダメージ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="d" />
-<br />
-基本ダメージ × 0.75
-= マイナス会心ダメージ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="e" />
-<br />
-基本ダメージ × 凶会心<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c8" value="0" />
-= 凶会心ダメージ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="f" />
-<hr />
-※ トレーニングエリアでのダメージを想定しています<br />
-※ 武器固有補正 武器SP倍率 状態異常補正 はシンプルにしたかったので、省略しています<br />
-※ 計算式は「ナウかる研究所」様の物を参考に作成しています。
+  <div id="ChoiceSkill">
+    <table>
+      <tr>
+        <th>スキル</th><th>装備レベル</th><th>効果</th><th>憑依錬成</th><th>武器スキル</th>
+      </tr>
+      <tr>
+        <td>攻撃</td>
+        <td><select><option>Lv 1</option><option>Lv 2</option><option>Lv 3</option></select></td>
+        <td></td>
+        <td><button>-</button><input type="text" value="0" readonly="true" /><button>+</button></td>
+        <td><button>-</button><input type="text" value="0" readonly="true" /><button>+</button></td>
+      </tr>
+      <tr>
+        <td>攻撃</td>
+        <td><select><option>Lv 1</option><option>Lv 2</option><option>Lv 3</option></select></td>
+        <td></td>
+        <td><button>-</button><input type="text" value="0" readonly="true" /><button>+</button></td>
+        <td><button>-</button><input type="text" value="0" readonly="true" /><button>+</button></td>
+      </tr>
+    </table>
   </div>
+  <hr />
+  <div>
+    <table>
+      <tr>
+        <th>攻撃力</th><th>属性値</th><th>肉質</th><th>モーション値</th>
+      </tr>
+      <tr>
+        <td><input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a1" /></td>
+        <td><input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b1" /></td>
+        <td><input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c6" value="130" /></td>
+        <td><input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c7" value="27" /></td>
+      </tr>
+    </table>
+    <hr />
+    ( 攻撃力 × 攻撃力<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a3" value="0" />%UP
+    + 攻撃力<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a4" value="0" />UP
+    + 錬成パラメータ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a5" value="0" />)
+    × 攻撃活性<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a6" value="0" />%UP
+    <hr />
+     ( 属性値 × 属性値<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b3" value="0" />%UP
+    + 属性値<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b4" value="0" />UP)
+    × 古龍属性<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b5" value="0" />%UP
+    <hr />
+    ( 物理攻撃力<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="a" />
+    + 属性攻撃力<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="b" />)
+    × ダメージ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c1" value="0" />%UP
+    × 肉質 / 100 × モーション値 / 100
+  <hr />
+  = 基本ダメージ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c" />
+  <br />
+  基本ダメージ × 会心倍率<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c3" value="1.25" />
+  = 会心ダメージ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="d" />
+  <br />
+  基本ダメージ × 0.75
+  = マイナス会心ダメージ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="e" />
+  <br />
+  基本ダメージ × 凶会心<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="c8" value="0" />
+  = 凶会心ダメージ<input type="text" size="4" maxlength="4" inputmode="numeric" class="inputNumeric" id="f" />
+  <hr />
+  ※ トレーニングエリアでのダメージを想定しています<br />
+  ※ 武器固有補正 武器SP倍率 状態異常補正 はシンプルにしたかったので、省略しています<br />
+  ※ 計算式は「ナウかる研究所」様の物を参考に作成しています。
+  </div>
+  <hr />
 </section>
 <section>
   <div id="copyRight">&copy;iKnowLab,</div>
